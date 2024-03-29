9712869b151fc33e92a96f99ca51bd03e9e171c8
GustavoGJesus <gustavogomesdev@proton.me>
2024-03-29 02:29:53 -0300

    fix: adjust hardhat config, rollux testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,9 @@ const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
 const SKIP_LOAD = process.env.SKIP_LOAD === 'true';
 const MAINNET_FORK = process.env.MAINNET_FORK === 'true';
 const FORKING_BLOCK = parseInt(process.env.FORKING_BLOCK || '11633164');
+const ANKR_API_KEY = process.env.ANKR_API_KEY;
+const ROLLUX_TANENBAUM_URL = process.env.ROLLUX_TANENBAUM_URL;
+const MNEMONIC = process.env.MNEMONIC;
 
 // Prevent to load scripts before compilation and typechain
 if (!SKIP_LOAD) {
@@ -32,6 +35,12 @@ if (!SKIP_LOAD) {
       });
   });
 }
+
+/*This will default to ROLLUX_TANENBAUM_URL if
+   you do not specify ANKR_API_KEY in .env */
+const rolluxTanenbaumUrl = ANKR_API_KEY
+  ? `https://rpc.ankr.com/rollux_testnet/${ANKR_API_KEY}`
+  : ROLLUX_TANENBAUM_URL;
 
 require(`${path.join(__dirname, 'tasks/misc')}/set-dre.ts`);
 
@@ -52,6 +61,13 @@ const config: HardhatUserConfig = {
           evmVersion: 'istanbul',
         },
       },
+      {
+        version: '0.8.0',
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+          evmVersion: 'istanbul',
+        },
+      },
     ],
   },
   typechain: {
@@ -59,18 +75,18 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      main: "abc" // Set to an empty string or some placeholder
+      main: 'abc', // Set to an empty string or some placeholder
     },
     customChains: [
       {
-        network: "main",
+        network: 'main',
         chainId: 57000,
         urls: {
-          apiURL: "https://rollux.tanenbaum.io/api",
-          browserURL: "https://rollux.tanenbaum.io/"
-        }
-      }
-    ]
+          apiURL: 'https://rollux.tanenbaum.io/api',
+          browserURL: 'https://rollux.tanenbaum.io/',
+        },
+      },
+    ],
   },
   defaultNetwork: 'hardhat',
   mocha: {
@@ -79,9 +95,14 @@ const config: HardhatUserConfig = {
   networks: {
     main: {
       chainId: 57000,
-      url: "https://rpc-tanenbaum.rollux.com",
-      accounts: [PRIVATE_KEY]
-    }, 
+      url: 'https://rpc-tanenbaum.rollux.com',
+      accounts: [PRIVATE_KEY],
+    },
+    'rollux-tanenbaum': {
+      url: rolluxTanenbaumUrl,
+      chainId: 57000,
+      accounts: { mnemonic: MNEMONIC },
+    },
     hardhat: {
       hardfork: 'istanbul',
       blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
